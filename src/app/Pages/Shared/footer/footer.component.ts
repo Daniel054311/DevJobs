8d735f638a591cf34de31a70464f7424fc3a5689
Daniel Forson <danielkojoforson27@gmail.com>
2024-02-14 14:20:14 +0100

    created the footer with it functionality

--- src/app/Pages/Shared/footer/footer.component.ts
+++ src/app/Pages/Shared/footer/footer.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Job } from '../../../job';
+import { ActivatedRoute } from '@angular/router';
+import { JobsService } from '../../../jobs.service';
+import { ThemeService } from '../../../theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  job: Job | undefined;
+
+  constructor(private route: ActivatedRoute, private jobsService: JobsService,
+    private themeService: ThemeService,) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const jobId = +params['id'];
+      this.jobsService.getJobDetails(jobId).subscribe(job => {
+        this.job = job;
+        console.log(job)
+      });
+    });
+  }
+
+
+  get isDarkMode(): boolean {
+    return this.themeService.getCurrentTheme();
+  }
 }
