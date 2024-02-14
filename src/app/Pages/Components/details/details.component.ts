24d8086b193d455386ab1505663755e5ff44c3d1
Daniel Forson <danielkojoforson27@gmail.com>
2024-02-14 14:19:47 +0100

    added css styles to a button in

--- src/app/Pages/Components/details/details.component.ts
+++ src/app/Pages/Components/details/details.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { JobsService } from '../../../jobs.service';
 import { ActivatedRoute } from '@angular/router';
 import { Job } from '../../../job';
+import { FooterComponent } from "../../Shared/footer/footer.component";
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+    selector: 'app-details',
+    standalone: true,
+    templateUrl: './details.component.html',
+    styleUrl: './details.component.css',
+    imports: [FooterComponent]
 })
 export class DetailsComponent implements OnInit{
 
