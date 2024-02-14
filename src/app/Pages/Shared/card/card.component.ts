6ab99fc948cafb74e70d0f6df6145d2410c51658
Daniel Forson <danielkojoforson27@gmail.com>
2024-02-14 14:19:14 +0100

    added filter functionality so that it can be sarched in the navbar component

--- src/app/Pages/Shared/card/card.component.ts
+++ src/app/Pages/Shared/card/card.component.ts
@@ -5,25 +5,28 @@ import { ThemeService } from '../../../theme.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
+
 @Component({
-  selector: 'app-card',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css',
+    selector: 'app-card',
+    standalone: true,
+    templateUrl: './card.component.html',
+    styleUrl: './card.component.css',
+    imports: [CommonModule]
 })
 export class CardComponent {
   jobs: Job[] = [];
 
+
   constructor(
     private router: Router,
     private jobsService: JobsService,
-    private themeService: ThemeService
+    private themeService: ThemeService,
   ) {}
 
   ngOnInit(): void {
     this.loadJobs();
   }
+
 
   loadJobs() {
     this.jobsService.getAllJobs().subscribe((jobs) => {
@@ -34,6 +37,30 @@ export class CardComponent {
   onSelectJob(jobId: number) {
     // Route user to details page with the ID of the selected job
     this.router.navigate(['/app-details', jobId]);
+  }
+
+  getFilteredJobs(): Job[] {
+    // Retrieve filter criteria from FilterService
+    const position = this.jobsService.position;
+    const location = this.jobsService.location;
+    const fullTimeOnly = this.jobsService.fullTimeOnly;
+
+    // Apply filtering logic based on filter criteria
+    let filteredJobs = this.jobs;
+
+    if (position) {
+      filteredJobs = filteredJobs.filter(job => job.position.toLowerCase().includes(position.toLowerCase()));
+    }
+
+    if (location) {
+      filteredJobs = filteredJobs.filter(job => job.location.toLowerCase().includes(location.toLowerCase()));
+    }
+
+    if (fullTimeOnly) {
+      filteredJobs = filteredJobs.filter(job => job.contract === 'Full Time');
+    }
+
+    return filteredJobs;
   }
 
   get isDarkMode(): boolean {
